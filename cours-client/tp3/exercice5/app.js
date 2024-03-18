864e3b49846acfa5f211361a589f2c58e0f21d1a
ManOfGod14 <maliki.tcherou1@gmail.com>
2024-03-18 12:07:51 +0000

    updated 20240318 1207

--- cours-client/tp3/exercice5/app.js
+++ cours-client/tp3/exercice5/app.js
@@ -5,18 +5,21 @@ const listElt = document.querySelector("#list");
 
 btnElt.addEventListener("click", function () {
   //   console.log(textElt.value);
+  if (textElt.value) {
+    // création d'un élement html en javascript
+    const newLi = document.createElement("li");
+    // console.log(newLi);
 
-  // création d'un élement html en javascript
-  const newLi = document.createElement("li");
-  // console.log(newLi);
+    // ajouter un contenu dans l'élement li crée
+    newLi.textContent = textElt.value;
+    // console.log(newLi);
 
-  // ajouter un contenu dans l'élement li crée
-  newLi.textContent = textElt.value;
-  // console.log(newLi);
+    // mettre l'element li crée et son contenu dans son parent ul
+    listElt.appendChild(newLi);
 
-  // mettre l'element li crée et son contenu dans son parent ul
-  listElt.appendChild(newLi);
-
-  // initialiser le champ input
-  textElt.value = "";
+    // initialiser le champ input
+    textElt.value = "";
+  } else {
+    alert("Veuillez remplir le champ input");
+  }
 });
