540142fca04d550c561b2088965b23e2827ee8b4
ManOfGod14 <maliki.tcherou1@gmail.com>
2024-04-01 12:40:25 +0000

    updated 20240401 1240

---
+++ cours-client/tp3/exercice15/assets/js/app.js
@@ -0,0 +1,19 @@
+const heuresIdElt = document.querySelector("#heuresId");
+const minutesIdElt = document.querySelector("#minutesId");
+const secondesIdElt = document.querySelector("#secondesId");
+const msecondesIdElt = document.querySelector("#millisecondesId");
+
+const startIdElt = document.querySelector("#startId");
+const stopIdElt = document.querySelector("#stopId");
+const resetIdElt = document.querySelector("#resetId");
+
+let timerId; // stocker l'id du timer
+let heure = 0;
+let minutes = 0;
+let secondes = 0;
+let msecondes = 0;
+
+// fonction pour afficher 2 chiffres lorsqu'on a 1 seul chiffre
+function numberFormat(nbr) {
+  return nbr.toString().padStart(2, "0");
+}
